42ad5e07d3230ab5ab49970bac9f0c5e873707e0
Zannatul Naim <zannatulnaim09@gmail.com>
2023-11-02 08:47:00 +0600

    updated

--- index.php
+++ index.php
@@ -4,21 +4,21 @@
     <title>Book Form</title>
 </head>
 <body>
+    <br><br>
     <div class="container">
-        <form action="./submit_book_data.php" method="post">
-            <label for="isbn">ISBN (13 characters):</label><br>
-            <input type="text" id="isbn" name="isbn" pattern=".{13,13}" required><br>
-            <label for="title">Title:</label><br>
-            <input type="text" id="title" name="title" required><br>
-            <label for="author">Author:</label><br>
-            <input type="text" id="author" name="author" required><br>
-            <label for="stock">Stock:</label><br>
-            <input type="number" id="stock" name="stock" min="1" step="1" required><br>
-            <label for="price">Price:</label><br>
-            <input type="number" id="price" name="price" min="0.01" step="0.01" required><br>
-            <input type="submit" value="Submit">
-        </form>
-        <br><br>
+       <center>
+            <pre>
+            <form action="./submit_book_data.php" method="post">
+                <label for="isbn">ISBN (13 characters) : </label> <input type="text" id="isbn" name="isbn" pattern=".{13,13}" required><br>
+                <label for="title">Title                : </label> <input type="text" id="title" name="title" required><br>
+                <label for="author">Author               : </label> <input type="text" id="author" name="author" required><br>
+                <label for="stock">Stock                : </label> <input type="number" id="stock" name="stock" min="1" step="1" required><br>
+                <label for="price">Price                : </label> <input type="number" id="price" name="price" min="0.01" step="0.01" required><br><br>
+                <input type="submit" value="Submit">
+            </form>
+            </pre>
+            <br><br>
+       </center>
     </div>
 </body>
 </html>
@@ -37,6 +37,7 @@ try {
     $stmt->execute();
 
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    echo "<center>";
     echo "<table border=1>";
     echo "<tr><th>Title</th><th>Author</th><th>ISBN</th><th>Stock</th><th>Price</th><th colspan=2>Action</th></tr>";
     foreach($stmt->fetchAll() as $k=>$v) {
@@ -50,6 +51,7 @@ try {
         echo "</tr>";
     }
     echo "</table>";
+    echo "</center>";
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
